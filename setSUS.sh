fca3f6a579222cc917af451eaa4aa05e96fc287d
bramgcohen <mail@bramgcohen.com>
2014-06-17 13:13:54 -0500

    Update setSUS.sh
    Updated for 10.9 support

--- setSUS.sh
+++ setSUS.sh
@@ -4,6 +4,7 @@
 #># With additions from Chad Brewer post on JamfNation
 
 #Adapted by Bram Cohen 2/20/13
+#Updated 6/13/2014 by Bram Cohen
 
 #Define your Snow Leopard Branch 10.6
 SLSUS="http://applesus.domain.com/content/catalogs/others/index-leopard-snowleopard.merged-1.sucatalog"
@@ -11,6 +12,9 @@ SLSUS="http://applesus.domain.com/content/catalogs/others/index-leopard-snowleop
 LSUS="http://servername/content/catalogs/others/index-lion-snowleopard-leopard.merged-1_Prod.sucatalog"
 #Define your Mountian Lion Branch 10.8
 MLSUS="http://servername/content/catalogs/others/index-mountainlion-lion-snowleopard-leopard.merged-1_Prod.sucatalog"
+#Define your MavericksBranch 10.9
+MVSUS="http://servername/content/catalogs/others/index-10.9-mountainlion-lion-snowleopard-leopard.merged-1_Prod.sucatalog"
+
 #Define the Name of your Server for the log output
 NAME="SERVERNAME"
 
@@ -36,6 +40,12 @@ case $OSversion in
 	defaults write /Library/Preferences/com.apple.softwareupdate CatalogURL $MLSUS
 	echo "Setting Sytem SUS to $NAME Mountain Lion Branch."
 	;;
+*10.9*)
+	defaults write /var/root/Library/Preferences/com.apple.softwareupdate CatalogURL $MVSUS
+	defaults write /Library/Preferences/com.apple.softwareupdate CatalogURL $MVSUS
+	defaults read /Library/Preferences/com.apple.softwareupdate CatalogURL
+	echo "Setting Sytem SUS to $NAME Mavericks Branch."
+	;;
 esac
 
 
@@ -46,10 +56,10 @@ do
     then
     
 case $OSversion in
-	
+
 *10.6*)
 	defaults write $d/Library/Preferences/com.apple.softwareupdate CatalogURL $SLSUS
-	echo "Setting SUS to $NAME Lion Branch for User $d"
+	echo "Setting SUS to $NAME Snow Leopard Branch for User $d"
 	;;
 *10.7*)
 	defaults write $d/Library/Preferences/com.apple.softwareupdate CatalogURL $LSUS
@@ -57,13 +67,18 @@ case $OSversion in
 	;;
 *10.8*)
 	defaults write $d/Library/Preferences/com.apple.softwareupdate CatalogURL $MLSUS
-	echo "Setting SUS to $NAME Lion Branch for User $d"
+	echo "Setting SUS to $NAME Mountian Lion Branch for User $d"
+	;;
+*10.9*)
+	defaults write $d/Library/Preferences/com.apple.softwareupdate CatalogURL $MVSUS
+	defaults read $d/Library/Preferences/com.apple.softwareupdate CatalogURL
+	echo "Setting SUS to $NAME Mavericks Branch for User $d"
 	;;
 esac
 
     else
         if [ -e "$d/Library/Preferences/.GlobalPreferences.plist" ];then
-        	cp /Library/Preferences/com.apple.softwareupdate.plsit $d/Library/Preferences/com.apple.softwareupdate.plsit
+        	cp /Library/Preferences/com.apple.softwareupdate.plsit $d/Library/Preferences/com.apple.softwareupdate.plist
         	echo "Setting SUS to $NAME for User $d"
         else
         	echo "The file was not found under $d skipping 
